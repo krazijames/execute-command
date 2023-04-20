eee649f7e27bad54ca4ed7864d654868c06a7bbf
krazijames <krazijames@gmail.com>
2023-04-20 15:30:53 +0900

    implement `ExecuteCommand` for `Command`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
 use std::process::{Command, ExitStatus, Output};
 use thiserror::Error;
 
+pub trait ExecuteCommand {
+    fn parse(command_string: impl AsRef<str>) -> Result<Command>;
+    fn execute_status(&mut self) -> Result<ExitStatus>;
+    fn execute_string(&mut self) -> Result<String>;
+}
+
+impl ExecuteCommand for Command {
+    fn parse(command_string: impl AsRef<str>) -> Result<Command> {
+        parse(command_string)
+    }
+
+    fn execute_status(&mut self) -> Result<ExitStatus> {
+        execute_status(self)
+    }
+
+    fn execute_string(&mut self) -> Result<String> {
+        execute_string(self)
+    }
+}
+
 pub fn parse(command_string: impl AsRef<str>) -> Result<Command> {
     let [program, args @ ..] = &shell_words::split(command_string.as_ref())?[..] else {
         return Ok(Command::new(""));
@@ -64,31 +84,48 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        let command = parse(r#"p a b"#).unwrap();
+        let command = Command::parse(r#"p a b"#).unwrap();
         assert_eq!(command.get_program(), "p");
         assert_eq!(command.get_args().collect::<Vec<_>>(), ["a", "b"]);
 
-        let command = parse(r#"p a "b c""#).unwrap();
+        let command = Command::parse(r#"p a "b c""#).unwrap();
         assert_eq!(command.get_program(), "p");
         assert_eq!(command.get_args().collect::<Vec<_>>(), ["a", "b c"]);
 
-        let command = parse(r#"p "a\"b""#).unwrap();
+        let command = Command::parse(r#"p "a\"b""#).unwrap();
         assert_eq!(command.get_program(), "p");
         assert_eq!(command.get_args().collect::<Vec<_>>(), ["a\"b"]);
 
-        let command = parse(r#"p "a"#);
+        let command = Command::parse(r#"p "a"#);
         assert!(command.is_err());
     }
 
     #[test]
-    fn test_status() {
-        assert!(status("bash -c 'exit 0'").is_ok());
-        assert!(status("bash -c 'exit 1'").is_err());
+    fn test_execute_status() {
+        assert!(Command::parse("bash -c 'exit 0'")
+            .unwrap()
+            .execute_status()
+            .is_ok());
+
+        assert!(Command::parse("bash -c 'exit 1'")
+            .unwrap()
+            .execute_status()
+            .is_err());
     }
 
     #[test]
-    fn test_string() {
-        assert_eq!(string("bash -c 'echo 1'").unwrap(), "1\n");
-        assert!(string("bash -c 'exit 1'").is_err());
+    fn test_execute_string() {
+        assert_eq!(
+            Command::parse("bash -c 'echo 1'")
+                .unwrap()
+                .execute_string()
+                .unwrap(),
+            "1\n"
+        );
+
+        assert!(Command::parse("bash -c 'exit 1'")
+            .unwrap()
+            .execute_string()
+            .is_err());
     }
 }
